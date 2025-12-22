568be795a0d3205374f9a514fdd516327f2d33cc
Biswajitdash-09 <biswajitdash929@gmail.com>
2025-12-22 22:58:02 +0530

    feat: add resizable panel UI components and a script to verify `react-resizable-panels` exports

---
+++ verify-exports.js
@@ -0,0 +1,5 @@
+const pkg = require('react-resizable-panels');
+console.log('Exports:', Object.keys(pkg));
+console.log('PanelGroup type:', typeof pkg.PanelGroup);
+console.log('Panel type:', typeof pkg.Panel);
+console.log('PanelResizeHandle type:', typeof pkg.PanelResizeHandle);
