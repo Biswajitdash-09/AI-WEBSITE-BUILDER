8ea60805957094a0cb435bfae6b532c9cd5579a5
Biswajitdash-09 <biswajitdash929@gmail.com>
2025-12-22 22:59:33 +0530

    chore: remove temporary build output and export verification script

--- verify-exports.js
+++
@@ -1,5 +0,0 @@
-const pkg = require('react-resizable-panels');
-console.log('Exports:', Object.keys(pkg));
-console.log('PanelGroup type:', typeof pkg.PanelGroup);
-console.log('Panel type:', typeof pkg.Panel);
-console.log('PanelResizeHandle type:', typeof pkg.PanelResizeHandle);
